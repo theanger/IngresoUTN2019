a28fb504fc9264c23f3b1ce97124d95e3cce9e98
theanger <31898638+theanger@users.noreply.github.com>
2019-02-12 22:34:22 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,7 +1,16 @@
 function Mostrar()
 {
 //tomo la edad  
+ var años;
+ var solo;
 
+ años= document.getElementById("edad").value;
+   solo=document.getElementById("estadoCivil").value;
+
+   if ( años<18 && solo== "Soltero")
+
+
+   alert ("Es muy menor para ser soltero.");
 	
 
 
