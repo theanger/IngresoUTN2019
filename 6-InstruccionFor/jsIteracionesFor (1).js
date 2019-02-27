f73c316e3c4cc1dc5f7cb089ab38b4371d1959ca
theanger <31898638+theanger@users.noreply.github.com>
2019-02-27 12:33:30 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,10 @@
 function Mostrar()
 {
+    
+
+for (var i=1 ; i<=10 ; i++) {
+
+    console.log(i);
+} 
 
 }
--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,8 +1,18 @@
 function Mostrar()
 {
 
+
 var repetciones = prompt("ingrese el número de repeticiones");
 
+while (isNaN( repeticiones)) {  
+
+repeticiones= parseInt ( prompt("eso no es correcto"));
+ }  
+
+for (var i = 0 ; i < repeticiones; i++) {
+     console.log ( "hola utn fra");
+}
 
 
+  
 }//FIN DE LA FUNCIÓN
